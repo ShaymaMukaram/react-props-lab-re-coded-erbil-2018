fd0efc67bae1c34c2267cc12dea897ac3ddfa8be
Shayma Mukaram <shaymamukaram@gmail.com>
2018-09-23 11:32:28 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,11 @@
-// Code The Spaceship Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+class Spaceship extends React.Component {
+  render() {
+    return (
+      
+     
+    );
+  }
+}
+ 
